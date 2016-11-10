4956bc3fcfadc9827268033a2718d83b939ac5e5
Gregor Woiwode <gw@co-it.eu>
2016-11-11 00:02:12 +0100

    Introduces component :book:-card

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'br-book',
-  templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  templateUrl: 'book.component.html',
+  styleUrls: ['book.component.scss']
 })
 export class BookComponent implements OnInit {
 
--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,11 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BookComponent } from './book.component';
+import { StoreModule }  from '@ngrx/store';
+
+import { ratingReducer }     from './reducers/rating.reducer';
+
+import { BookComponent }     from './book.component';
+import { BookCardComponent } from './book-card/book-card.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.provideStore({ rating: ratingReducer })
   ],
-  declarations: [BookComponent]
+  declarations: [BookComponent, BookCardComponent]
 })
 export class BookModule { }
