a0ab37420346e33bea2a9c31946819a211b1509b
Gregor Woiwode <gw@co-it.eu>
2016-11-28 10:01:41 +0100

    References EffectsModule

--- src/app/book/book.module.ts
+++ src/app/book/book.module.ts
@@ -1,11 +1,16 @@
-import { NgModule }     from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule }             from '@angular/core';
+import { CommonModule }         from '@angular/common';
 import { ReactiveFormsModule }  from '@angular/forms';
 
 import { StoreModule }         from '@ngrx/store';
+import { EffectsModule }       from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { reducer } from './reducers';
+
+import { BookEffects } from './effects/book';
+
+import { BooksService } from './services/books.service';
 
 import { BookComponent }     from './book.component';
 import { BookCardComponent } from './book-card/book-card.component';
@@ -17,8 +22,10 @@ import { BookListComponent } from './book-list/book-list.component';
     CommonModule,
     ReactiveFormsModule,
     StoreModule.provideStore(reducer),
-    StoreDevtoolsModule.instrumentOnlyWithExtension()
+    StoreDevtoolsModule.instrumentOnlyWithExtension(),
+    EffectsModule.run(BookEffects)
   ],
+  providers: [BooksService],
   exports: [BookComponent],
   declarations: [BookComponent, BookCardComponent, BookFormComponent, BookListComponent]
 })
