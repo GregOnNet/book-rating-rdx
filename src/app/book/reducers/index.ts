fc69144b722d469b1f9a5d2e91b8eb43851912bb
Gregor Woiwode <gw@co-it.eu>
2016-11-28 11:40:24 +0100

    Adds store freeze

--- src/app/book/reducers/index.ts
+++ src/app/book/reducers/index.ts
@@ -2,7 +2,10 @@ import { Observable } from 'rxjs';
 import { compose } from '@ngrx/core';
 import { ActionReducer, combineReducers } from '@ngrx/store';
 
+import { storeFreeze } from 'ngrx-store-freeze';
+
 import * as fromBooks from './books';
+import { environment } from './../../../environments/environment';
 
 export interface State {
   books: fromBooks.State;
@@ -12,11 +15,16 @@ const reducers = {
   books: fromBooks.reducer
 };
 
+const developmentReducer: ActionReducer<State> = compose(storeFreeze, combineReducers)(reducers);
 const productionReducer: ActionReducer<State> = combineReducers(reducers);
 
 export function reducer(state: any, action: any) {
-  return productionReducer(state, action);
-};
+  if (environment.production) {
+    return productionReducer(state, action);
+  }
+
+  return developmentReducer(state, action);
+}
 
 export function getBookState(state$: Observable<State>) {
   return state$.select(state => state.books);
